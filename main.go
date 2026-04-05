6c88bc570d6e9bedc3f889295c5566937c798f0a
Abdulfetah <suudiabdulfetah@gmail.com>
2026-04-05 15:01:11 +0300

    Read 8 byte chunks

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	f, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal("error", "error", err)
+	}
+
+	data := make([]byte, 8)
+	for {
+		n, err := f.Read(data)
+		if err != nil {
+			break
+		}
+		fmt.Printf("read: %s\n", string(data[:n]))
+	}
 }
